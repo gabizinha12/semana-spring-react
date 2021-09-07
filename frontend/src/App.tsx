0d2b28259eba01b8848ca213f6a9bbc1d6340533
gabizinha12 <gabrielle.plima1965@gmail.com>
2021-09-07 14:51:01 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "assets/components/DataTable";
+import Footer from "assets/components/Footer";
+import Navbar from "assets/components/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Olá mundo</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 

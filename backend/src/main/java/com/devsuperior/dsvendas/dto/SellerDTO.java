37a59088c592be31fbddc71fa99f8cf353dff8e7
gabizinha12 <gabrielle.plima1965@gmail.com>
2021-09-10 14:47:41 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -0,0 +1,46 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SellerDTO  implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public SellerDTO(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public SellerDTO(Seller entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public SellerDTO() {
+		
+	}
+}

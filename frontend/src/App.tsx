8ff162d0e2326d11eb8009cde3892d6b7d52e98b
gabizinha12 <gabrielle.plima1965@gmail.com>
2021-09-12 00:12:22 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,7 @@
-import BarChart from "assets/components/BarChart";
-import DataTable from "assets/components/DataTable";
-import DonutChart from "assets/components/DonutChart";
-import Footer from "assets/components/Footer";
-import Navbar from "assets/components/Navbar";
+import Routes from "Routes";
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
-        <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>
-        </div>
-        <DataTable />
-      </div>
-      <Footer />
-    </>
-  );
+  return <Routes />;
 }
 
 export default App;
